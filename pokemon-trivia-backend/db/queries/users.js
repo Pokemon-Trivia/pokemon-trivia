b0a4bb586d56720e3f92cae7c49fc3f27db638a4
Jeremiah <jeremiah.torbit@gmail.com>
2026-02-27 12:07:31 -0700

    Fix package.json, add user function

--- pokemon-trivia-backend/db/queries/users.js
+++ pokemon-trivia-backend/db/queries/users.js
@@ -1,7 +1,18 @@
-import db from '../client.js'
+import db from '../client.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+await db.connect();
 
 export const createUser = async({username, password, highScore}) => {
    const sql = `
       INSERT INTO users (username, password, high_score)
-   `
+      VALUES ($1, $2, $3)
+      RETURNING username
+   `;
+   const securePassword = await bcrypt.hash(password, 10)
+   const {rows: [user]} = await db.query(sql, [username, securePassword, highScore]);
+   const token = jwt.sign({user}, process.env.JWT_SECRET)
+   console.log(token);
+   return token;
 }
