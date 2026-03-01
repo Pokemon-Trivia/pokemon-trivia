a06abaefbe9b0c235064f89eceb51a5066f127a1
nataliayatsishin <nata.yatsishin@gmail.com>
2026-02-28 20:52:59 -0600

    Create Register Route and test in in app.js

---
+++ pokemon-trivia-backend/api/users.js
@@ -0,0 +1,22 @@
+import express from "express";
+const router = express.Router();
+export default router;
+
+import { createUser } from "../db/queries/users.js";
+
+/* POST /users/register */
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).send("Username and password are missing.");
+    }
+
+    const token = await createUser({ username, password });
+
+    res.status(201).send(token);
+  } catch (error) {
+    next(error);
+  }
+});
