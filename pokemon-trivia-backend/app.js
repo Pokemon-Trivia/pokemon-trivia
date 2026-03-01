f1825eb52b37b43ed2ace4d4f40f53a1133411e9
nataliayatsishin <nata.yatsishin@gmail.com>
2026-02-28 21:06:23 -0600

    Handle duplicate username error and generic error fallback

--- pokemon-trivia-backend/app.js
+++ pokemon-trivia-backend/app.js
@@ -7,3 +7,15 @@ import userRouter from "./api/users.js";
 app.use(express.json());
 
 app.use("/users", userRouter);
+
+app.use((err, req, res, next) => {
+  if (err.code === "23505") {
+    return res.status(409).send("Username already exist.");
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong.");
+});
