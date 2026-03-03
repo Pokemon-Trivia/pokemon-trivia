f1cbcc315fc9436f78046217f798c1a1a6b9bb70
nataliayatsishin <nata.yatsishin@gmail.com>
2026-03-02 22:19:17 -0600

    Create login route and adding findUserByUsername function

--- pokemon-trivia-backend/api/users.js
+++ pokemon-trivia-backend/api/users.js
@@ -2,7 +2,11 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-import { createUser } from "../db/queries/users.js";
+import { createUser, findUserByUsername } from "../db/queries/users.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.JWT_SECRET;
 
 /* POST /users/register */
 router.post("/register", async (req, res, next) => {
@@ -16,6 +20,36 @@ router.post("/register", async (req, res, next) => {
     const token = await createUser({ username, password });
 
     res.status(201).send({ token });
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* POST /users/login */
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).send("Username and password are missing.");
+    }
+
+    const foundUser = await findUserByUsername(username);
+    if (!foundUser) {
+      return res.status(401).send("Invalid credentials.");
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, foundUser.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).send("Invalid credentials.");
+    }
+
+    const token = jwt.sign(
+      { id: foundUser.id, username: foundUser.username },
+      SECRET,
+    );
+
+    res.status(200).send({ token });
   } catch (error) {
     next(error);
   }
