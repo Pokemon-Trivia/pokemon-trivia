bf555c10a37d3793d61b6ce82893118ceb8ce2cd
Jeremiah <jeremiah.torbit@gmail.com>
2026-03-03 20:51:29 -0700

    Add initial style for question and score component, updating gameboard styling and images

--- pokemon-trivia-frontend/src/question-answer/QuestionBoard.jsx
+++ pokemon-trivia-frontend/src/question-answer/QuestionBoard.jsx
@@ -42,7 +42,7 @@ const QuestionBoard = () => {
       setTypes(await getTypes());
    }
 
-   const answerShuffle = (answerArray, rightAnswer) => {
+   const answerShuffle = (answerArray) => {
       for (let i = answerArray.length -1; i > 0; i--) {
          const j = Math.floor(Math.random() * (i + 1));
          [answerArray[i], answerArray[j]] = [answerArray[j], answerArray[i]];
