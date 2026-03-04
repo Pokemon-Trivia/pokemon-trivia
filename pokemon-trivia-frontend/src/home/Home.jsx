9146ce2d4b1a3daae9ba67e57b05ee5a0a51b3e3
nataliayatsishin <nata.yatsishin@gmail.com>
2026-03-03 22:09:18 -0600

    create Home.jsx

--- pokemon-trivia-frontend/src/home/Home.jsx
+++ pokemon-trivia-frontend/src/home/Home.jsx
@@ -1,15 +1,23 @@
 import { useNavigate } from "react-router";
 import { useAuth } from "../auth/AuthContext";
 import { useEffect } from "react";
+import pokeball from "../assets/pokeball.png";
 
 export default function Home() {
   const { token } = useAuth();
   const navigate = useNavigate();
-  useEffect(() => {
-    if (!token) {
-      navigate("/login");
-    }
-  }, [token, navigate]);
+  // useEffect(() => {
+  //   if (!token) {
+  //     navigate("/login");
+  //   }
+  // }, [token, navigate]);
 
-  return <h1>Welcome to Pokémon Trivia</h1>;
+  return (
+    <div className="homePage">
+      <div className="homeHeader">
+        <img src={pokeball} alt="pokeball logo" id="pokeLogo" />
+        <h1>Welcome to Pokémon Trivia</h1>
+      </div>
+    </div>
+  );
 }
