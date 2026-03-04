a3a81ceae59e80a2c1876936b16b320a6ac1ba24
Jeremiah <jeremiah.torbit@gmail.com>
2026-03-03 22:34:52 -0700

    Add two buttons, play again to play game again, home to navigate home

--- pokemon-trivia-frontend/src/question-answer/PlayOrHome.jsx
+++ pokemon-trivia-frontend/src/question-answer/PlayOrHome.jsx
@@ -1,5 +1,13 @@
-const PlayOrHome = () => {
-   return <h1>Loading...</h1>
+import { useNavigate } from "react-router";
+
+const PlayOrHome = ({resetGame}) => {
+   const navigate = useNavigate()
+   return (
+      <section id="play-or-home">
+         <p onClick={() => resetGame()}>Play Again</p>
+         <p onClick={navigate('/home')}>Home</p>
+      </section>
+   )
 }
 
 export default PlayOrHome;
