74ad1d6454fded85052f72d4f829ae1dda9f32d4
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-03-06 23:39:51 -0700

    Add Friends Header component

---
+++ pokemon-trivia-frontend/src/friends/FriendsHeader.jsx
@@ -0,0 +1,10 @@
+const FriendsHeader = () => {
+   return (
+      <section id="friend-header">
+         <h2>Friends</h2>
+         <p>Total Friends: #</p>
+      </section>
+   )
+}
+
+export default FriendsHeader;
