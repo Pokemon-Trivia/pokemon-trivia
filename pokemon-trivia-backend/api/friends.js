21793b768d6e4ed478594a50a8d9f658bb97815f
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-03-07 12:06:47 -0700

    Add get route to friends.js

---
+++ pokemon-trivia-backend/api/friends.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+const friendsRouter = express.Router();
+export default friendsRouter;
+
+friendsRouter.get('/', (req, res, next) => {
+   try {
+      const token = req.headers.authorization.split(' ')[1];
+      const user = jwt.verify(token, process.env.JWT_SECRET)
+      console.log(user)
+   } catch (error) {
+      
+   }
+})
--- pokemon-trivia-frontend/src/friends/Friends.jsx
+++ pokemon-trivia-frontend/src/friends/Friends.jsx
@@ -5,8 +5,22 @@ import FriendsList from "./FriendsList";
 import { useAuth } from "../auth/AuthContext";
 
 const Friends = () => {
-  const [friends, setFriends] = useState([]);
-  const { token } = useAuth();
+   const [friends, setFriends] = useState([]);
+   const { token } = useAuth();
+
+   const getFriends = async () => {
+      const response = await fetch("/friends", {
+         method: "GET",
+         headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+         }
+      })
+   }
+   
+   const tryGetFriends = async () => {
+      
+   }
 
   return (
     <section id="friends">
