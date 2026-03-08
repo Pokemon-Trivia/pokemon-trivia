1e7e66bf86d75a5ab0539599428aa4cdbf097690
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-03-07 23:59:24 -0700

    Add section id for styling, style back button

--- pokemon-trivia-frontend/src/friends/FoundFriends.jsx
+++ pokemon-trivia-frontend/src/friends/FoundFriends.jsx
@@ -1,14 +1,17 @@
 const FoundFriends = ({ foundFriends }) => {
    return foundFriends.length > 0 ? (
-      <ul>
-         {foundFriends.map((friend) => {
-            return <FoundFriendsLi friend={friend} />
-         })}
-      </ul>
+      <section id="found-friends">
+         <ul>
+            {foundFriends.map((friend) => {
+               return <FoundFriendsLi friend={friend} />
+            })}
+         </ul>
+         <button id="back-btn">Back</button>
+      </section>
    )
    : <h2>No Trainers Found</h2>
 }
 
-const FoundFriendsLi = ({ friend }) => <li>{friend} <button id="add-friend">Add Friend</button></li>
+const FoundFriendsLi = ({ friend }) => <li>{friend} <button id="add-friend-btn">Add Friend</button></li>
 
 export default FoundFriends;
