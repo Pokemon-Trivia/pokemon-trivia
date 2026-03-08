3073316af364d0b6c42d239980bf7156f23efa54
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-03-07 21:46:46 -0700

    Fix more styling issues

--- pokemon-trivia-frontend/src/layout/Layout.jsx
+++ pokemon-trivia-frontend/src/layout/Layout.jsx
@@ -5,11 +5,11 @@ import { useAuth } from "../auth/AuthContext";
 export default function Layout() {
   const { token } = useAuth();
   return (
-    <>
+    <section id="root-children">
       {token && <Navbar />}
       <main className="pageContent">
         <Outlet />
       </main>
-    </>
+    </section>
   );
 }
