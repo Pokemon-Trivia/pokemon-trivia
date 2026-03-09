fe10b7b0c04548c730518726a95d7b8de7b93e9c
nataliayatsishin <nata.yatsishin@gmail.com>
2026-03-09 15:58:49 -0500

    add account route

--- pokemon-trivia-frontend/src/App.jsx
+++ pokemon-trivia-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./index.css";
 import Register from "./auth/Register.jsx";
 import Login from "./auth/Login.jsx";
 import Home from "./home/Home.jsx";
+import Account from "./account/Account.jsx";
 import Layout from "./layout/Layout.jsx";
 
 import { Routes, Route } from "react-router";
@@ -14,10 +15,11 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/question" element={<QuestionBoard />} />
+      <Route path="/account" element={<Account />} />
       <Route element={<Layout />}>
         <Route path="/home" element={<Home />} />
         <Route path="/leaderboard" element={<h1>Leaderboard</h1>} />
-        <Route path="/friends" element={<Friends  />} />
+        <Route path="/friends" element={<Friends />} />
       </Route>
     </Routes>
   );
