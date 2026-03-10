b3fa2a5e2786c475a750dd821a1c9cc35d714225
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-03-09 19:30:50 -0600

    Add try get leaders function to run on page load, if no leaders in state, show loading element

--- pokemon-trivia-frontend/src/leaderboard/Leaderboard.jsx
+++ pokemon-trivia-frontend/src/leaderboard/Leaderboard.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import LeaderList from "./LeaderList";
+import { getLeaders } from "../api/leaders";
 
 const Leaderboard = () => {
-   return (
+   const [leaders, setLeaders] = useState([]);
+
+   const tryGetLeaders = async () => {
+      const topLeaders = await getLeaders();
+      console.log("Component: ", topLeaders)
+      setLeaders(topLeaders)
+   }
+
+   useEffect(() => {
+      tryGetLeaders();
+   }, [])
+
+   return leaders.length > 0 ? (
       <section id="leaderboard-component">
          <h1 id="leader-header">Top Trainers!</h1>
          <LeaderList />
       </section>
    )
+   : <p>Loading...</p>
 }
 
 export default Leaderboard;
