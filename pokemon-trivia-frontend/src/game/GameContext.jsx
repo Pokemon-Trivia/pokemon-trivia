6d10395efdcc895c978becf70967887d427dfa6f
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-03-11 18:11:44 -0600

    Add exports to the functions for use

--- pokemon-trivia-frontend/src/game/GameContext.jsx
+++ pokemon-trivia-frontend/src/game/GameContext.jsx
@@ -8,4 +8,10 @@ const GameProvider = ({ children }) => {
 
 export const useGame = () => {
    const context = useContext(GameContext)
+   if (!context) {
+      throw new Error("Must have access to GameProvider to use")
+   }
+   return context;
 }
+
+export default GameProvider;
