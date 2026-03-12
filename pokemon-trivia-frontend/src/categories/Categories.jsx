1bac263be626a5963091cbeaf6da89f298102c0f
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-03-12 16:17:29 -0600

    Add borders and pseudo effects when hovering and selecting category

--- pokemon-trivia-frontend/src/categories/Categories.jsx
+++ pokemon-trivia-frontend/src/categories/Categories.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router";
 import { useGame } from "../game/GameContext";
 import { useEffect } from "react";
+import "./categories.css"
 
 const Categories = () => {
    const { gameCategory, setGameCategory } = useGame();
@@ -15,10 +16,10 @@ const Categories = () => {
 
    return gameCategory < 1 ? (
       <section id="categories-section">
-         <p id="type-btn" onClick={() => {
+         <p className="cat-btn" onClick={() => {
             setGameCategory(1);
          }} >Pokémon Types</p>
-         <p id="name-btn" onClick={() => {
+         <p className="cat-btn" onClick={() => {
             setGameCategory(2);
          }}>Pokémon Names</p>
       </section>
